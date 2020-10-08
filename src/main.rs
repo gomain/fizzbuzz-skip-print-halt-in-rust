1504f074b3093b859198e4937fac571246925e08
Joe Gomain Hoyes <gomain@gmail.com>
2020-10-08 21:55:31 +0700

    use iterator instead of for loop

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use fizzbuzz::{program, satisfy, Condition };
+use fizzbuzz::{program, Condition};
 
 fn main() {
     let rules = vec![(3, "Fizz"), (5, "Buzz"), (7, "Hizz"), (11, "Howl")]
@@ -6,11 +6,9 @@ fn main() {
         .map(|(n, word)| (Condition::MultipleOf(n), word))
         .collect();
 
-    for n in 1.. {
-        print!("{}:\t", n);
-        program(n, &rules).run();
-        if rules.iter().all(|(cond, _)| satisfy(cond, n)) {
-            break;
-        }
-    }
+    (1..1156) // 1155 is the first number that prints all words!
+        .for_each(|n| {
+            print!("{}:\t", n);
+            program(n, &rules).run();
+        })
 }
