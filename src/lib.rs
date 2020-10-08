716a3bcd4b5c54c71391ef745f8baf62f183367e
Joe Gomain Hoyes <gomain@gmail.com>
2020-10-08 21:29:00 +0700

    refactor to lib.rs

---
+++ src/lib.rs
@@ -0,0 +1,140 @@
+pub fn program<'a>(n: u32, rules: &'a Rules) -> Program<'a> {
+    rules
+        .into_iter()
+        .filter(|(cond, _)| satisfy(cond, n))
+        .fold(
+            ProgramWithHole::new(Program::skip(), Program::print_num(n) + Program::halt()),
+            |cur, (_, word)| {
+                cur + ProgramWithHole::new(Program::print_string(word), Program::halt())
+            },
+        )
+        .join()
+}
+
+pub type Rules<'a> = Vec<(Condition, &'a str)>;
+
+#[derive(Clone, Debug)]
+enum Cmd<'a> {
+    Skip,
+    Halt,
+    PrintString(&'a str),
+    PrintNum(u32),
+}
+
+#[derive(Clone, Debug)]
+pub struct Program<'a> {
+    cmds: Vec<Cmd<'a>>,
+}
+
+impl<'a> Program<'a> {
+    pub fn run(&self) {
+        for cmd in &self.cmds {
+            match cmd {
+                Cmd::Skip => continue,
+                Cmd::Halt => {
+                    println!("");
+                    return;
+                }
+                Cmd::PrintString(it) => print!("{}", it),
+                Cmd::PrintNum(n) => print!("{}", n),
+            }
+        }
+    }
+
+    fn single(cmd: Cmd<'a>) -> Program<'a> {
+        Program { cmds: vec![cmd] }
+    }
+
+    fn halt() -> Program<'a> {
+        Program::single(Cmd::Halt)
+    }
+
+    fn skip() -> Program<'a> {
+        Program::single(Cmd::Skip)
+    }
+
+    fn print_string(s: &'a str) -> Program<'a> {
+        Program::single(Cmd::PrintString(s))
+    }
+
+    fn print_num(n: u32) -> Program<'a> {
+        Program::single(Cmd::PrintNum(n))
+    }
+}
+
+use std::ops;
+
+impl<'a> ops::Add for Program<'a> {
+    type Output = Self;
+    fn add(self, other: Self) -> Self {
+        Program {
+            cmds: self
+                .cmds
+                .into_iter()
+                .chain(other.cmds.into_iter())
+                .collect(),
+        }
+    }
+}
+
+impl<'a> ops::Add for &'a Program<'a> {
+    type Output = Program<'a>;
+    fn add(self, other: &'a Program) -> Program<'a> {
+        self.clone() + other.clone()
+    }
+}
+
+impl<'a> ops::Add<&'a Program<'a>> for Program<'a> {
+    type Output = Self;
+    fn add(self, other: &'a Program) -> Self {
+        self + other.clone()
+    }
+}
+
+#[derive(Debug)]
+struct ProgramWithHole<'a> {
+    pre: Program<'a>,
+    post: Program<'a>,
+}
+
+impl<'a> ProgramWithHole<'a> {
+    fn new(pre: Program<'a>, post: Program<'a>) -> Self {
+        ProgramWithHole { pre, post }
+    }
+
+    fn join(self) -> Program<'a> {
+        self.pre + self.post
+    }
+}
+
+impl<'a> ops::Add for ProgramWithHole<'a> {
+    type Output = Self;
+    fn add(self, other: Self) -> Self {
+        ProgramWithHole::new(self.pre + other.pre, other.post + self.post)
+    }
+}
+
+impl<'a> ops::Add for &'a ProgramWithHole<'a> {
+    type Output = ProgramWithHole<'a>;
+    fn add(self, other: &'a ProgramWithHole) -> ProgramWithHole<'a> {
+        self.clone() + other.clone()
+    }
+}
+
+impl<'a> ops::Add<&'a Program<'a>> for &'a ProgramWithHole<'a> {
+    type Output = Program<'a>;
+    fn add(self, other: &'a Program) -> Program<'a> {
+        self.pre.clone() + other + self.post.clone()
+    }
+}
+
+#[derive(Debug)]
+pub enum Condition {
+    MultipleOf(u32),
+}
+
+pub fn satisfy(cond: &Condition, i: u32) -> bool {
+    match cond {
+        Condition::MultipleOf(n) => i % n == 0,
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
+use fizzbuzz::{program, satisfy, Condition };
+
 fn main() {
-    let rules: Rules = vec![(3, "Fizz"), (5, "Buzz"), (7, "Hizz"), (11, "Howl")]
-        .iter()
-        .map(|(n, word)| (Condition::MultipleOf(*n), word.to_string()))
+    let rules = vec![(3, "Fizz"), (5, "Buzz"), (7, "Hizz"), (11, "Howl")]
+        .into_iter()
+        .map(|(n, word)| (Condition::MultipleOf(n), word))
         .collect();
 
     for n in 1.. {
@@ -10,146 +12,5 @@ fn main() {
         if rules.iter().all(|(cond, _)| satisfy(cond, n)) {
             break;
         }
-    }
-}
-
-fn program(n: u32, rules: &Rules) -> Program {
-    rules
-        .into_iter()
-        .filter(|(cond, _)| satisfy(cond, n))
-        .fold(
-            ProgramWithHole::new(Program::skip(), Program::print_num(n) + Program::halt()),
-            |cur, (_, word)| {
-                cur + ProgramWithHole::new(Program::print_string(word.clone()), Program::halt())
-            },
-        )
-        .join()
-}
-
-type Rules = Vec<(Condition, String)>;
-
-#[derive(Clone, Debug)]
-enum Cmd {
-    Skip,
-    Halt,
-    PrintString(String),
-    PrintNum(u32),
-}
-
-#[derive(Clone, Debug)]
-struct Program {
-    cmds: Vec<Cmd>,
-}
-
-impl Program {
-    fn run(&self) {
-        for cmd in &self.cmds {
-            match cmd {
-                Cmd::Skip => continue,
-                Cmd::Halt => {
-                    println!("");
-                    return;
-                }
-                Cmd::PrintString(it) => print!("{}", it),
-                Cmd::PrintNum(n) => print!("{}", n),
-            }
-        }
-    }
-
-    fn single(cmd: Cmd) -> Program {
-        Program { cmds: vec![cmd] }
-    }
-
-    fn halt() -> Program {
-        Program::single(Cmd::Halt)
-    }
-
-    fn skip() -> Program {
-        Program::single(Cmd::Skip)
-    }
-
-    fn print_string(s: String) -> Program {
-        Program::single(Cmd::PrintString(s))
-    }
-
-    fn print_num(n: u32) -> Program {
-        Program::single(Cmd::PrintNum(n))
-    }
-}
-
-use std::ops;
-
-impl<'a> ops::Add for Program {
-    type Output = Self;
-    fn add(self, other: Self) -> Self {
-        Program {
-            cmds: self
-                .cmds
-                .into_iter()
-                .chain(other.cmds.into_iter())
-                .collect(),
-        }
-    }
-}
-
-impl ops::Add for &Program {
-    type Output = Program;
-    fn add(self, other: &Program) -> Program {
-        self.clone() + other.clone()
-    }
-}
-
-impl<'a> ops::Add<&Program> for Program {
-    type Output = Self;
-    fn add(self, other: &Program) -> Program {
-        self + other.clone()
-    }
-}
-
-#[derive(Debug)]
-struct ProgramWithHole {
-    pre: Program,
-    post: Program,
-}
-
-impl ProgramWithHole {
-    fn new(pre: Program, post: Program) -> Self {
-        ProgramWithHole { pre, post }
-    }
-
-    fn join(self) -> Program {
-        self.pre + self.post
-    }
-}
-
-impl ops::Add for ProgramWithHole {
-    type Output = Self;
-    fn add(self, other: Self) -> Self {
-        ProgramWithHole::new(self.pre + other.pre, other.post + self.post)
-    }
-}
-
-impl ops::Add for &ProgramWithHole {
-    type Output = ProgramWithHole;
-    fn add(self, other: &ProgramWithHole) -> ProgramWithHole {
-        self.clone() + other.clone()
-    }
-}
-
-impl ops::Add<&Program> for &ProgramWithHole {
-    type Output = Program;
-    fn add(self, other: &Program) -> Program {
-        self.pre.clone() + other + self.post.clone()
-    }
-}
-
-#[derive(Debug)]
-enum Condition {
-    MultipleOf(u32),
-}
-
-fn satisfy(cond: &Condition, i: u32) -> bool {
-    match cond {
-        Condition::MultipleOf(n) => i % n == 0,
     }
 }
